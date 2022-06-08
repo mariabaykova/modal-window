57334fc4e482313c4f87328cc1ad75cba1104d3f
Maria Baykova <mnbaikova@gmail.com>
2022-06-08 07:40:00 +0500

    can be applied to two or more buttons but there is a problem with messages

--- index.js
+++ index.js
@@ -1,61 +1,92 @@
 const wrapperElem = document.querySelector(".wrapper");
-const modalWindowElem = document.querySelector("#modal-window-id");
+// const modalWindowElem = document.querySelector("#modal-window-id");
 const buttonElem = document.querySelector("#button-open");
+const buttonElem1 = document.querySelector("#button-open1");
 const shadowElem = document.querySelector("#wrapper-shadow");
 const bodyElem = document.querySelector("body");
 
+let modalWindowElem;
+
+const buttonElems = document.querySelectorAll(".button");
+console.log("buttonElems " + buttonElems);
+
+const modalWindowMessages = {
+    "button-open1" :
+    {
+        id: "button-open1",
+        message : "Кнопка нажата, может быть что-то произойдет, ждите."
+    },
+    "button-open" : { 
+        id: "button-open", 
+        message: "Заявка успешно отправлена! Менеджер с вами свяжется."
+    },
+};
+
 shadowElem.style.display = "none";
 
-buttonElem.addEventListener( "click", (event) => {
-    let btnTarget = event.target;
-    clearBlock(modalWindowElem);
-    openModalWindow(modalWindowElem, "Сообщение в модальное окно");
+let btnTarget;
+let btn;
+for ( let i = 0; i < buttonElems.length; i++ ) {
+    btn = buttonElems[i];
+    console.log("btn " + btn.id);
+    console.log("modalWindowMessages[btn.id][message] " + modalWindowMessages[btn.id]["message"] );
+    btn.addEventListener( "click", handleButtonClick);
+        
+}
+
+function handleButtonClick( e ) {
+    console.log("создание окна с сообщением " + modalWindowMessages[btn.id]["message"]);
+    btnTarget = e.target;
+    modalWindowElem = openModalWindow(modalWindowMessages[btn.id]["message"]);
 
     // включить прослушку клика всего документа
-    document.addEventListener( "click", ( e ) => {
-        let target = e.target;
-        // первое условие проверяет, есть ли блок в нашем окне, по идее, если нет, то мы не вызывали его,
-        // btnTarget != target - а это исключает немедленное закрытие нашего окна по событию "клик по кнопке", которое по сути является кликом вне модального окна
-        if ( !isEmpty(modalWindowElem) && ( btnTarget != target ) ) {
-            if ( modalWindowElem.contains(target) ) {
-                // alert("Click inside target element!");
-                // кликнули по модальному окну, ничего не делать пока
-              } else {
-                // alert("Click outside target element!");
-                clearBlock( modalWindowElem );
-                closeModalWindow( modalWindowElem );
-                // убрать прослушку всего документа
-                document.removeEventListener('click', (event) => {});
-              }
-        }
-    } 
-    );
+    document.addEventListener( "click", handleDocumentClick);
+
 }
-);
+
+function handleDocumentClick( e1 ) {
+    let target = e1.target;
+    if ( modalWindowElem && ( btnTarget != target ) ) {
+        if ( modalWindowElem.contains(target) ) {
+            alert(" Click inside target element! Do nothing");
+            // кликнули по модальному окну, ничего не делать пока
+          } else {
+            alert(" Click outside target element! I will close the window and remove document listener");
+            closeModalWindow( modalWindowElem );
+            // убрать прослушку всего документа
+            document.removeEventListener('click', handleDocumentClick);
+          }
+    }
+
+}
 
 // открыть модальное окно
-function openModalWindow( elem, message ) {
+function openModalWindow( message ) {
+    // создать окно
+    const modalWin = document.createElement("div");
+    modalWin.classList.add("modal-window");
+    modalWin.classList.add("modal-window_open");
+
     // затемнить страницу
     shadowElem.style.display = "";
     // отменить прокрутку
     bodyElem.classList.add("scroll-cancel");
 
-    // elem.classList.add("message");
     const messageBox = document.createElement("span");
     messageBox.classList.add("message");
 
     messageBox.textContent = message;
-    elem.appendChild(messageBox);
+    modalWin.appendChild(messageBox);
 
-    // показать модальное окно
-    elem.classList.add("modal-window_open");
+      //добавить окно в документ
+      wrapperElem.appendChild(modalWin);
 
-
+      return modalWin;
 }
 // закрыть модальное окно
 function closeModalWindow( elem ) { 
     // окно убрать
-    elem.classList.remove("modal-window_open");
+    elem.remove();
      // растемнить страницу
     shadowElem.style.display = "none";
     // включить прокрутку
@@ -63,14 +94,18 @@ function closeModalWindow( elem ) {
 }
 
 // очистка блока
-function clearBlock( block ) {
-    let blockLength = block.children.length;
-    for ( let ii = 0; ii < blockLength; ii++ ) {
-        block.removeChild(block.lastElementChild);
-    }
-}
+// function clearBlock( block ) {
+//     if ( block ) {
+        
+    
+//         let blockLength = block.children.length;
+//         for ( let ii = 0; ii < blockLength; ii++ ) {
+//             block.removeChild(block.lastElementChild);
+//         }
+//     }        
+// }
 
 // поверка на наличие дочерних блоков элемента
-function isEmpty( block ) {
-    return !block.children.length;
-}
+// function isEmpty( block ) {
+//     return !block.children.length;
+// }
